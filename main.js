2738f03196e89bd558574188d4f9e7bff4b14ce4
robloxer qwertydododododo <129184282+YAHAVITHECODER@users.noreply.github.com>
2023-10-01 20:01:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function preload()
+{
+ classifier = ml5.imageClassifier('doodleNet');
+}
+
+function setup()
+{
+    canvas = createCanvas(280, 280);
+    canvas.position(530, 200);
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function draw()
+{
+    strokeWeight(7);
+    stroke(0)
+    stroke_color("#975252");
+
+    if(mouseIsPressed)
+    {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+        
+    }
+}
+
+function Cc()
+{
+    background("white");
+}
+
+function classifyCanvas()
+{
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, result)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    console.log(result)
+
+    document.getElementById("labelL").innerHTML ="label:" + result[0].label;
+    document.getElementById("labelC").innerHTML ="confidence:" + Math.round(result[0].confidence) * 100 + "%";
+
+    utterThis = new SpeechSynthesisUtterance(result[0].label);
+    synth.speak(utterThis);
+}
